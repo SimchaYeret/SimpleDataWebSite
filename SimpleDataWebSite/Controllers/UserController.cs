e9171cc91c9d3dd49843179d3c7bb157bceb5d0c
Simcha Yeret <simchey770@gmail.com>
2021-12-15 00:20:45 +0200

    Added a HttpGet to get UserModel

--- SimpleDataWebSite/Controllers/UserController.cs
+++ SimpleDataWebSite/Controllers/UserController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleDataWebSite.Models;
 
 namespace SimpleDataWebSite.Controllers
 {
     public class UserController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new List<UserModel>());
         }
     }
 }
