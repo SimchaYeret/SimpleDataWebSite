d9254a48a149bface7caab3445171e2a2d0086ed
Simcha Yeret <simchey770@gmail.com>
2021-12-15 00:49:14 +0200

    Added HttpPost to get data from file

--- SimpleDataWebSite/Controllers/UserController.cs
+++ SimpleDataWebSite/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SimpleDataWebSite.Models;
 
@@ -13,6 +16,29 @@ namespace SimpleDataWebSite.Controllers
         public IActionResult Index()
         {
             return View(new List<UserModel>());
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection form)
+        {
+            List<UserModel> users = new List<UserModel>();
+            var fileName = "./Users.xlsx";
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read())
+                    {
+                        users.Add(new UserModel
+                        {
+                            Name = reader.GetValue(0).ToString(),
+                            Phone = reader.GetValue(1).ToString()
+                        });
+                    }
+                }
+            }
+            return View(users);
         }
     }
 }
